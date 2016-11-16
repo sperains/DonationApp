cae07c39d7393d1af434f8e238c1d5195048a1cd
rains <chenai0122@gmail.com>
2016-11-16 21:46:17 +0800

    update entry.js history

--- app/entry.js
+++ app/entry.js
@@ -1,12 +1,12 @@
-require('es5-shim');
-require('es5-shim/es5-sham');
-require('console-polyfill');
+// require('es5-shim');
+// require('es5-shim/es5-sham');
+// require('console-polyfill');
 
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import  {Router , Route , IndexRoute , Redirect} from 'react-router';
-import createHistory from 'history/lib/createHashHistory';
+import  {Router , Route , IndexRoute , Redirect , hashHistory} from 'react-router';
+//import createHistory from 'history/lib/createHashHistory';
 import Main from './containers/Main';
 import store from './store.js';
 
@@ -17,11 +17,11 @@ let root = $('#container')[0];
 
 
 
-const history = createHistory();
+// const history = createHistory();
 
 ReactDOM.render(
     <Provider store={store}>
-    	<Router history={history}>
+    	<Router history={hashHistory}>
     		<Route  path="/" component={Main}/>
     	</Router>
     </Provider>,
